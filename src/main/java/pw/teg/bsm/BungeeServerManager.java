19718b81ddc40b906d09513bbe398fc776097828
BuildTools <unconfigured@null.spigotmc.org>
2015-09-26 10:12:36 -0400

    Add API to remove, add and check if servers exist

--- src/main/java/pw/teg/bsm/BungeeServerManager.java
+++ src/main/java/pw/teg/bsm/BungeeServerManager.java
@@ -3,20 +3,28 @@ package pw.teg.bsm;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
+import pw.teg.bsm.api.ServerManagerAPI;
 import pw.teg.bsm.commands.ServerManagerCommand;
 
 public class BungeeServerManager extends Plugin {
 
     private static BungeeServerManager instance;
+    private static ServerManagerAPI api;
 
     @Override
     public void onEnable() {
         instance = this;
+
+        api = new ServerManagerAPI();
 
         getProxy().getPluginManager().registerCommand(this, new ServerManagerCommand());
     }
 
     public static BungeeServerManager get() {
         return instance;
+    }
+
+    public static ServerManagerAPI getApi() {
+        return api;
     }
 }
---
+++ src/main/java/pw/teg/bsm/api/ServerManagerAPI.java
@@ -0,0 +1,49 @@
+package pw.teg.bsm.api;
+
+import net.md_5.bungee.BungeeServerInfo;
+import net.md_5.bungee.api.config.ServerInfo;
+import pw.teg.bsm.api.events.ServerAddEvent;
+import pw.teg.bsm.api.events.ServerRemoveEvent;
+import pw.teg.bsm.util.ServerHelper;
+
+import java.net.InetSocketAddress;
+
+public class ServerManagerAPI {
+
+    public boolean addServer(String name, InetSocketAddress ipAddress, String motd, boolean restricted) {
+        if (ServerHelper.getServerInfo(name) != null) {
+            return false;
+        }
+
+        ServerInfo serverInfo = new BungeeServerInfo(name, ipAddress, motd, restricted);
+        ServerAddEvent addEvent = new ServerAddEvent(serverInfo, null);
+
+        if (addEvent.isCancelled()) {
+            return false;
+        }
+
+        ServerHelper.addServer(addEvent.getServerModified());
+        return true;
+    }
+
+    public boolean removeServer(String name) {
+        ServerInfo serverInfo = ServerHelper.getServerInfo(name);
+
+        if (serverInfo == null) {
+            return false;
+        }
+
+        ServerRemoveEvent addEvent = new ServerRemoveEvent(serverInfo, null);
+
+        if (addEvent.isCancelled()) {
+            return false;
+        }
+
+        ServerHelper.removeServer(addEvent.getServerModified().getName());
+        return true;
+    }
+
+    public boolean serverExists(String name, boolean checkCase) {
+        return ServerHelper.serverExists(name, checkCase);
+    }
+}
