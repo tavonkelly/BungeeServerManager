9751773db40ba84eed18a9ffed2454fb26369acb
Tavon <tavonk.tk@gmail.com>
2021-01-05 23:06:04 -0500

    Don't use InetSocketAddress' toString method

--- src/main/java/pw/teg/bsm/util/ConfigHelper.java
+++ src/main/java/pw/teg/bsm/util/ConfigHelper.java
@@ -1,5 +1,6 @@
 package pw.teg.bsm.util;
 
+import io.netty.channel.unix.DomainSocketAddress;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
@@ -8,6 +9,8 @@ import net.md_5.bungee.config.YamlConfiguration;
 import pw.teg.bsm.BungeeServerManager;
 
 import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.util.concurrent.TimeUnit;
 
 public class ConfigHelper {
@@ -35,8 +38,20 @@ public class ConfigHelper {
             return;
         }
 
+        SocketAddress socketAddress = serverInfo.getSocketAddress();
+        String addressString;
+
+        if (socketAddress instanceof DomainSocketAddress) {
+            addressString = "unix:" + ((DomainSocketAddress) socketAddress).path();
+        } else if (socketAddress instanceof InetSocketAddress) {
+            InetSocketAddress inetAddress = (InetSocketAddress) socketAddress;
+            addressString = inetAddress.getAddress().getHostAddress() + ":" + inetAddress.getPort();
+        } else {
+            addressString = socketAddress.toString();
+        }
+
         bungeeConfig.set("servers." + serverInfo.getName() + ".motd", serverInfo.getMotd().replace(ChatColor.COLOR_CHAR, '&'));
-        bungeeConfig.set("servers." + serverInfo.getName() + ".address", serverInfo.getSocketAddress().toString());
+        bungeeConfig.set("servers." + serverInfo.getName() + ".address", addressString);
         bungeeConfig.set("servers." + serverInfo.getName() + ".restricted", false);
         saveConfig();
     }
